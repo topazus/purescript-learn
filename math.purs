2362d18a0063db8b70a2eb784fcc8fd2af46f2e2
Felix Wang <77263945+topazus@users.noreply.github.com>
2022-06-24 11:27:11 +0800

    Update math.purs

--- math.purs
+++ math.purs
@@ -11,8 +11,13 @@ sum = \x y -> x + y
 add ∷ ∀ (a12 ∷ Type). Semiring a12 ⇒ a12 → a12 → a12
 add x y = x + y
 
+diagonal_ a b = sqrt (a * a + b * b)
+
 diagonal ∷ Number → Number → Number
-diagonal a b = sqrt (a * a + b * b)
+diagonal a b = res
+  where
+  t = max a b
+  res = t * (sqrt (a / t) * (a / t) + (b / t) * (b / t))
 
 circleArea ∷ Number → Number
 circleArea r = pi * r * r
